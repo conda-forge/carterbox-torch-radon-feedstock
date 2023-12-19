cc7b502f93a21d215f2d706dd474933ad938624b
Daniel Ching <carterbox@users.noreply.github.com>
2023-12-18 18:44:01 -0600

    BUG: Update CUDA arch list for new archs

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 set -ex
 
-export TORCH_CUDA_ARCH_LIST="3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6+PTX"
+if [[ ${cuda_compiler_version} == 11.2 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6+PTX"
+elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 8.9+PTX"
+elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 9.0+PTX"
+else
+    echo "Unsupported CUDA version. Edit build.sh!"
+    exit 1
+fi
 
 $PYTHON -m pip install . -vv --no-deps
