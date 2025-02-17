5e1930d9eb5448eb9528f6bd7ab502d7c97504a0
Daniel Ching <carterbox@users.noreply.github.com>
2025-02-17 11:58:34 -0600

    BLD: Add c compiler to requirements

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,7 @@
 @echo on
 
+setlocal enabledelayedexpansion
+
 if "%cuda_compiler_version%"=="11.2" (
     set "TORCH_CUDA_ARCH_LIST=3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6+PTX"
 ) else if "%cuda_compiler_version%"=="11.8" (
@@ -14,6 +16,8 @@ if "%cuda_compiler_version%"=="11.2" (
     echo Unsupported CUDA version. Edit build.bat!
     exit /b 1
 )
+
+endlocal & set "TORCH_CUDA_ARCH_LIST=%TORCH_CUDA_ARCH_LIST%"
 
 %PYTHON% -m pip install . -vv --no-deps
 if errorlevel 1 exit /b 1
