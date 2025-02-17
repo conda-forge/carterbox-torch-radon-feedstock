013d945d6f887a987bf6a3898fbbbb2b0bb0b965
Daniel Ching <carterbox@users.noreply.github.com>
2025-02-17 11:16:28 -0600

    BLD: Update arch lists

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,19 @@
 @echo on
 
-set TORCH_CUDA_ARCH_LIST="3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6+PTX"
-if errorlevel 1 exit /b 1
+if "%cuda_compiler_version%"=="11.2" (
+    set "TORCH_CUDA_ARCH_LIST=3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6+PTX"
+) else if "%cuda_compiler_version%"=="11.8" (
+    set "TORCH_CUDA_ARCH_LIST=3.5 3.7 5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 8.9+PTX"
+) else if "%cuda_compiler_version%"=="12.0" (
+    set "TORCH_CUDA_ARCH_LIST=5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 9.0+PTX"
+) else if "%cuda_compiler_version%"=="12.6" (
+    set "TORCH_CUDA_ARCH_LIST=5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 9.0+PTX"
+) else if "%cuda_compiler_version%"=="12.8" (
+    set "TORCH_CUDA_ARCH_LIST=5.0 5.2 5.3 6.0 6.1 6.2 7.0 7.2 7.5 8.0 8.6 9.0 12.0+PTX"
+) else (
+    echo Unsupported CUDA version. Edit build.bat!
+    exit /b 1
+)
 
 %PYTHON% -m pip install . -vv --no-deps
 if errorlevel 1 exit /b 1
